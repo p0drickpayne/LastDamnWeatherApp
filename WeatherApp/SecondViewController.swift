49e34b42e33080592d52083bac540183f5fb7097
jakob salomonsson <jakob@salomonssons.se>
2020-02-21 15:46:14 +0100

    Initial Commit

---
+++ WeatherApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WeatherApp
+//
+//  Created by Jakob Salomonsson on 2020-02-21.
+//  Copyright © 2020 Jakob Salomonsson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
